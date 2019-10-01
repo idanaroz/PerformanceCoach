aebeb14ab916654cce5e405cfd1515e4a723ef9d
idanaroz <idanaroz@gmail.com>
2019-10-01 15:18:48 +0300

    Initial commit

---
+++ src/main/java/com/example/PerformanceCoach/PerformanceCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.PerformanceCoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceCoachApplication.class, args);
+	}
+
+}
