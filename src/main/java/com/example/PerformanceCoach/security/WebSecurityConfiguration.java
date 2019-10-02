14b1ad0eccfc5ef9f677b7e86b7748d3648f8b88
idanaroz <idanaroz@gmail.com>
2019-10-02 10:53:24 +0300

    adding password encoder

--- src/main/java/com/example/PerformanceCoach/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/PerformanceCoach/security/WebSecurityConfiguration.java
@@ -1,19 +1,30 @@
 package com.example.PerformanceCoach.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("idannaroz@gmail.com").password("1234").roles("USER");
+        auth.inMemoryAuthentication().
+                passwordEncoder(getPasswordEncoder()).
+                withUser("idannaroz@gmail.com").
+                password(getPasswordEncoder().encode("1234")).
+                roles("USER");
 //        super.configure(auth);
     }
 
